f481494fe289ae435fccfbb2eb3ac00441c2ba91
林欣榮 <jansen.sin188@gmail.com>
2017-09-28 03:10:21 +0800

    Complete HW2.25

---
+++ 2.25/source/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	printf(" SS     SS  \n");
+	printf("  SS   SS  SS\n");
+	printf("    SSS     SS\n");
+	printf("\n");
+	printf(" ZZZZ     ZZ\n");
+	printf(" ZZ   ZZ  ZZ\n");
+	printf(" ZZ 	ZZZZ\n");
+	printf("\n");
+	printf(" UUUUUUUUUU \n");
+	printf(" U          \n");
+	printf(" UUUUUUUUUU \n");
+	printf("\n");
+	printf("      -     \n");
+	printf("      -     \n");
+	printf("      -     \n");
+	printf("      -     \n");
+	printf("\n");
+	printf("  CCCCCCCC  \n");
+	printf(" C        C \n");
+	printf("C          C\n");
+	printf("\n");
+	printf("HHHHHHHHHHHH\n");
+	printf("     HH     \n");
+	printf("HHHHHHHHHHHH\n");
+	printf("\n");
+	printf("I          I\n");
+	printf("IIIIIIIIIIII\n");
+	printf("I          I\n");
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
